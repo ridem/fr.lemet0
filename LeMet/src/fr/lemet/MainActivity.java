8fffb3dc0491fdf47645239fb15b8a7df8484e78
Mehdi Rejraji <mehdi.rejraji@gmail.com>
2013-04-16 16:24:28 +0200

    Exemple commit

--- LeMet/src/fr/lemet/MainActivity.java
+++ LeMet/src/fr/lemet/MainActivity.java
@@ -80,6 +80,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        System.out.println("gdfgd");
         return true;
     }
     
