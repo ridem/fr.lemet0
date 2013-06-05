dd19808f12f95b020adc09f07610a8a661367e62
ridem <mehdi.rejraji@gmail.com>
2013-06-05 15:44:38 +0200

    TouchImageView

--- LeMet/src/fr/lemet/PlanReseau.java
+++ LeMet/src/fr/lemet/PlanReseau.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class PlanReseau extends Activity {
-
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plan_reseau);
+        TouchImageView img = (TouchImageView) findViewById(R.id.testm);
+        img.setImageResource(R.drawable.testm);
+        img.setMaxZoom(4f);
     }
-}
+
+    }
