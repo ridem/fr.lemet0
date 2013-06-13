64816d7aa4a338426467295ee4af8d9103916bfa
Guillaume_Moumou <g.mourozeau@gmail.com>
2013-06-13 14:46:13 +0200

    Ajout plan des lignes Bouton Ligne de bus 1 : en horizontal Bouton Ligne de bus 2 : la même en vertical Ce sont les vraies lignes LeMet

--- LeMet/src/fr/lemet/PlanReseau.java
+++ LeMet/src/fr/lemet/PlanReseau.java
@@ -5,16 +5,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class PlanReseau extends Activity {
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plan_reseau);
-        TouchImageView img = (TouchImageView) findViewById(R.id.testm);
-        img.setImageResource(R.drawable.testm);
-        img.setMaxZoom(4f);
 
+        // Instanciation du WebView...
+        WebView wvSite = (WebView)findViewById(R.id.webview);
 
+        //...on active JavaScript...
+        WebSettings webSettings = wvSite.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //...et on charge la page
+        wvSite.loadUrl("file:///");
     }
 }
