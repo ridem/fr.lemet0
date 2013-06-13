00123464cbf48d742186737765b967c41bf8edff
ridem <mehdi.rejraji@gmail.com>
2013-06-13 14:02:14 +0200

    Merge branch 'master' of /home/ridem/AndroidStudioProjects/fr.lemet with conflicts.

--- LeMet/src/fr/lemet/PlanReseau.java
+++ LeMet/src/fr/lemet/PlanReseau.java
@@ -1,20 +1,15 @@
 package fr.lemet;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class PlanReseau extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plan_reseau);
-        TouchImageView img = (TouchImageView) findViewById(R.id.testm);
+        TouchImageView img = (TouchImageView) findViewById(R.id.plansortie);
         img.setImageResource(R.drawable.testm);
         img.setMaxZoom(4f);
-
-
     }
 }
