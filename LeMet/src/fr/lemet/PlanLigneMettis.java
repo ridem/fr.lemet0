a6cd8adcb0b1998cfa1b13f7146213ff012b4782
Guillaume_Moumou <g.mourozeau@gmail.com>
2013-06-14 17:03:41 +0200

    modif ligne mettis

--- LeMet/src/fr/lemet/PlanLigneMettis.java
+++ LeMet/src/fr/lemet/PlanLigneMettis.java
@@ -14,15 +14,19 @@ public class PlanLigneMettis extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plan_lignemettis);
         WebView planweb = (WebView)findViewById(R.id.webview);
+        planweb.setInitialScale(115);
+        //Display display = getWindowManager().getDefaultDisplay();
+        //int width=display.getWidth();
         WebSettings planSettings = planweb.getSettings();
         planSettings.setSupportZoom(true);
         planSettings.setBuiltInZoomControls(true);
-        planSettings.setLoadWithOverviewMode(true);
+        //planSettings.setLoadWithOverviewMode(true);
         planSettings.setUseWideViewPort(true);
-        //planSettings.setDefaultZoom(WebSettings.ZoomDensity.CLOSE);
+        //planSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
         planweb.loadUrl("file:///android_asset/planmettis.png");
         //planSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         //planweb.loadDataWithBaseURL("file:///android_asset/","<html><center><img src=\"planlemet.png\" ali></html>","text/html","utf-8","");
-        //planweb.loadDataWithBaseURL("file:///android_asset/","<html><body><img src=\"planlemet.png\" /></body></html>" ,"text/html",  "UTF-8","");
+        //planweb.loadDataWithBaseURL("file:///android_asset/","<html><head><style type='text/css'>body{margin:O 0; text-align:center;} img{border:0px; text-align:center;} </style></head><body><img src=\"planlemet.png\" /></body></html>" ,"text/html",  "UTF-8","");
+
     }
 }
