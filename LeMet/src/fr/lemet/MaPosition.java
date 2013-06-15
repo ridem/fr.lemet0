2b1fb7d248a228519c98fee81a44d850e8283de6
Guillaume_Moumou <g.mourozeau@gmail.com>
2013-06-15 13:44:33 +0200

    Ajout MaPosition Modif Horaires

---
+++ LeMet/src/fr/lemet/MaPosition.java
@@ -0,0 +1,12 @@
+package fr.lemet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MaPosition extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ma_position);
+    }
+}
--- LeMet/src/fr/lemet/MainActivity.java
+++ LeMet/src/fr/lemet/MainActivity.java
@@ -9,75 +9,76 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-            public final static String plans = "com.example.lemet.plans";
-            public final static String horaires = "com.example.lemet.horaires";
-            public final static String itineraire = "com.example.lemet.itineraire";
+    public final static String plans = "com.example.lemet.plans";
+    public final static String horaires = "com.example.lemet.horaires";
+    public final static String itineraire = "com.example.lemet.itineraire";
+    public final static String position = "com.example.lemet.position";
 
+    private Button passerellePlans = null;
+    private Button passerelleHoraires = null;
+    private Button passerelleItineraire = null;
+    private Button passerellePosition = null;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            private Button passerellePlans = null;
-            private Button passerelleHoraires = null;
-            private Button passerelleItineraire = null;
-
-
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-
-                passerellePlans = (Button) findViewById(R.id.plans);
-                passerelleHoraires = (Button) findViewById(R.id.horaires);
-                passerelleItineraire = (Button) findViewById(R.id.itineraire);
-
-
+        passerellePlans = (Button) findViewById(R.id.plans);
+        passerelleHoraires = (Button) findViewById(R.id.horaires);
+        passerelleItineraire = (Button) findViewById(R.id.itineraire);
+        passerellePosition = (Button) findViewById(R.id.maposition);
 
         passerellePlans.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                // Le premier paramètre est le nom de l'activité actuelle
-                // Le second est le nom de l'activité de destination
-                Intent activitePlans = new Intent(MainActivity.this, Plans.class);
+              // Le premier paramètre est le nom de l'activité actuelle
+              // Le second est le nom de l'activité de destination
+              Intent activitePlans = new Intent(MainActivity.this, Plans.class);
 
-                // On rajoute un extra
-                activitePlans.putExtra(plans, 31);
+              // On rajoute un extra
+              activitePlans.putExtra(plans, 31);
 
-                // Puis on lance l'intent
-                startActivity(activitePlans);
+              // Puis on lance l'intent
+              startActivity(activitePlans);
 
             }
         });
 
-        passerelleHoraires.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent activiteHoraires = new Intent(MainActivity.this, Horaires.class);
-                activiteHoraires.putExtra(horaires, 2);
-                startActivity(activiteHoraires);
-            }
-        });
+         passerelleHoraires.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 // TODO Auto-generated method stub
+                 Intent activiteHoraires = new Intent(MainActivity.this, Horaires.class);
+                 activiteHoraires.putExtra(horaires, 2);
+                 startActivity(activiteHoraires);
+             }
+         });
 
-        passerelleItineraire.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
+         passerelleItineraire.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View arg0) {
                 // TODO Auto-generated method stub
                 Intent activiteItineraire = new Intent(MainActivity.this, Itineraire.class);
                 activiteItineraire.putExtra(itineraire, 3);
                 startActivity(activiteItineraire);
+             }
+         });
+
+         passerellePosition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Le premier paramètre est le nom de l'activité actuelle
+                // Le second est le nom de l'activité de destination
+                Intent activitePosition = new Intent(MainActivity.this, MaPosition.class);
+
+                // On rajoute un extra
+                activitePosition.putExtra(position, 31);
+
+                // Puis on lance l'intent
+                startActivity(activitePosition);
+
             }
-        });
-
-
+         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    // Tests
-
 }
